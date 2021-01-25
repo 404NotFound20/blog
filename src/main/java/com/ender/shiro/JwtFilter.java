4e625441c8818ac1c9a4acdc5035fe29e3821373
404NotFound20 <1376041660@qq.com>
2021-01-25 22:47:07 +0800

    跨域处理

--- src/main/java/com/ender/shiro/JwtFilter.java
+++ src/main/java/com/ender/shiro/JwtFilter.java
@@ -9,9 +9,11 @@ import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.ExpiredCredentialsException;
 import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
+import org.apache.shiro.web.util.WebUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -23,7 +25,7 @@ import java.io.IOException;
  * 定义jwt的过滤器JwtFilter。
  * 这个过滤器是我们的重点，这里我们继承的是Shiro内置的AuthenticatingFilter，一个可以内置了可以自动登录方法的的过滤器，有些同学继承BasicHttpAuthenticationFilter也是可以的。
  * 我们需要重写几个方法：
- *
+ * <p>
  * createToken：实现登录，我们需要生成我们自定义支持的JwtToken  根据请求信息构建一个token
  * onAccessDenied：拦截校验，当头部没有Authorization时候，我们直接通过，不需要自动登录；当带有的时候，首先我们校验jwt的有效性，没问题我们就直接执行executeLogin方法实现自动登录
  * onLoginFailure：登录异常时候进入的方法，我们直接把异常信息封装然后抛出
@@ -34,40 +36,47 @@ import java.io.IOException;
 public class JwtFilter extends AuthenticatingFilter {
     @Autowired
     JwtUtils jwtUtils;
+
     @Override
     protected AuthenticationToken createToken(ServletRequest servletRequest, ServletResponse servletResponse) throws Exception {
-        HttpServletRequest request=(HttpServletRequest) servletRequest;
-        String jwt=request.getHeader("Authorization");
-        //未获取到jwt就直接返回，禁止登录
-        if(StringUtils.isEmpty(jwt)){
+        log.info("判断是否存在jwt 是否构建token返回");
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String jwt = request.getHeader("Authorization");
+        if (StringUtils.isEmpty(jwt)) {
+            log.info("获取到jwt就直接返回，禁止登录");
             return null;
         }
-        //根据请求信息构建token
+        log.info("根据请求信息构建token");
         return new JwtToken(jwt);
     }
-   //校验有效性，是否超时等等
+
+    //校验有效性，是否超时等等
     @Override
     protected boolean onAccessDenied(ServletRequest servletRequest, ServletResponse servletResponse) throws Exception {
-        HttpServletRequest request=(HttpServletRequest) servletRequest;
-        String jwt=request.getHeader("Authorization");
-        if(StringUtils.isEmpty(jwt)){
-            //没有就不需要登录，交给注解处理权限
+        log.info("校验有效性，是否超时等等");
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String jwt = request.getHeader("Authorization");
+        if (StringUtils.isEmpty(jwt)) {
+            log.info("请求没有带jwt，不需要登录，交给注解处理权限");
             return true;
-        }else {
+        } else {
             //有就校验jwt合法性
+            log.info("请求带了jwt，需要登录，校验有效性，是否超时等等");
             Claims claim = jwtUtils.getClaimByToken(jwt);
-            if(claim == null || jwtUtils.isTokenExpired(claim.getExpiration())) {
+            if (claim == null || jwtUtils.isTokenExpired(claim.getExpiration())) {
                 throw new ExpiredCredentialsException("token已失效，请重新登录");
             }
             //进行登录
             return executeLogin(servletRequest, servletResponse);
         }
     }
+
     @Override
     protected boolean onLoginFailure(AuthenticationToken token, AuthenticationException e, ServletRequest request, ServletResponse response) {
 
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-
+        log.info("onLoginFailure  ？？？？？？？？？？？？？？");
         Throwable throwable = e.getCause() == null ? e : e.getCause();
         Result result = Result.fail(throwable.getMessage());
         String json = JSONUtil.toJsonStr(result);
@@ -75,8 +84,28 @@ public class JwtFilter extends AuthenticatingFilter {
         try {
             httpServletResponse.getWriter().print(json);
         } catch (IOException ioException) {
-           log.error(ioException.getMessage());
+            log.error(ioException.getMessage());
         }
         return false;
+    }
+
+    /**
+     * 进行登录校验前先进行跨域处理
+     */
+    @Override
+    protected boolean preHandle(ServletRequest request, ServletResponse response) throws Exception {
+        log.info("进行登录校验前先进行跨域处理");
+        HttpServletRequest httpServletRequest = WebUtils.toHttp(request);
+        HttpServletResponse httpServletResponse = WebUtils.toHttp(response);
+        httpServletResponse.setHeader("Access-control-Allow-Origin", httpServletRequest.getHeader("Origin"));
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", httpServletRequest.getHeader("Access-Control-Request-Headers"));
+        log.info("跨域时会首先发送一个OPTIONS请求，这里我们给OPTIONS请求直接返回正常状态");
+        if (httpServletRequest.getMethod().equals(RequestMethod.OPTIONS.name())) {
+            httpServletResponse.setStatus(org.springframework.http.HttpStatus.OK.value());
+            return false;
+        }
+
+        return super.preHandle(request, response);
     }
 }
