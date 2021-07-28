78b34e61f55690ee42b16754905b43a6fad370a3
ender <>
2021-07-29 00:52:32 +0800

    切换中间件

--- src/main/java/com/ender/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ender/common/exception/GlobalExceptionHandler.java
@@ -20,20 +20,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
 
-
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(value = ShiroException.class)
     public Result handler(ShiroException e) {
-        log.error("运行时异常：----------------{}", e);
+        log.error("运行时异常：{ }", e);
         return Result.fail(401, e.getMessage(), null);
     }
+
     /**
-     *     实体校验异常
+     * 实体校验异常
      */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public Result handler(MethodArgumentNotValidException e) {
-        log.error("实体校验异常：----------------{}", e);
+        log.error("实体校验异常：{ }", e);
         BindingResult bindingResult = e.getBindingResult();
         ObjectError objectError = bindingResult.getAllErrors().stream().findFirst().get();
         return Result.fail(objectError.getDefaultMessage());
@@ -42,14 +42,14 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = IllegalArgumentException.class)
     public Result handler(IllegalArgumentException e) {
-        log.error("Assert异常：----------------{}", e);
+        log.error("Assert异常：{ }", e);
         return Result.fail(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = RuntimeException.class)
     public Result handler(RuntimeException e) {
-        log.error("运行时异常：----------------{}", e);
+        log.error("运行时异常：{ }", e);
         return Result.fail(e.getMessage());
     }
 
