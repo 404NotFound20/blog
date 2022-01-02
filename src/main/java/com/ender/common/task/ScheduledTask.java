2e3943ce153a103de674ce0607841697844bafd4
404NotFound20 <1376041660@qq.com>
2022-01-03 01:47:05 +0800

    短信定时任务

---
+++ src/main/java/com/ender/common/task/ScheduledTask.java
@@ -0,0 +1,21 @@
+package com.ender.common.task;
+
+import cn.hutool.core.date.DateUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Classname ScheduledTask
+ * @Description TODO
+ * @Date 2022/1/3 1:21 上午
+ * @Created by zenghui
+ */
+@Component
+@Slf4j
+public class ScheduledTask {
+    @Scheduled(cron = "*/5 * * * * ?")
+    public void dailySms(){
+        log.info("今天的短信是：「」  发送时间{}", DateUtil.now());
+    }
+}
